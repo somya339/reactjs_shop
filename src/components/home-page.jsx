00843299c8a7988ae726549679ecccd2375cf21b
Somya Gupta <somyagupta746@gmail.com>
2020-12-27 12:45:21 +0530

    this is the initial commit to the project with the basic setup

---
+++ src/components/home-page.jsx
@@ -0,0 +1,47 @@
+import { React } from "react";
+import "./homepage.styles.scss"
+const Home = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-items">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-items">
+        <div className="content">
+          <h1 className="title">JEANS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-items">
+        <div className="content">
+          <h1 className="title">JACKET</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-items">
+        <div className="content">
+          <h1 className="title">WOMEN</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-items">
+        <div className="content">
+          <h1 className="title">MEN</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+export default Home;
